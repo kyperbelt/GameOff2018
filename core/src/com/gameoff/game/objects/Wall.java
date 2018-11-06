a9e54e57b22d31219986370818ac775ae351f28c
Grant-StrayVoltage <grant@fivemedia.ca>
2018-11-06 05:30:44 -0400

    quick fix for programatically created Walls with no Map properties- null check

--- core/src/com/gameoff/game/objects/Wall.java
+++ core/src/com/gameoff/game/objects/Wall.java
@@ -11,8 +11,13 @@ public class Wall extends Basic {
 	@Override
 	public void init(MapProperties properties) {
 		super.init(properties);
-
-		//if no passable bool property found then it is set to false
-		getMove().setPassable(properties.get("passable",false,Boolean.class));
+    if (properties == null)
+    {
+      getMove().setPassable(false);
+    } else
+    {
+      //if no passable bool property found then it is set to false
+      getMove().setPassable(properties.get("passable",false,Boolean.class));
+    }
 	}
 }
