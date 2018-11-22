c31b7c98c41727d6961f92c9f146ed8b3b6d40e6
jonathan <jonathancamarenacamcho@gmail.com>
2018-11-22 07:34:24 -0800

    animated health WOO!

--- core/src/com/gameoff/game/managers/OverlayManager.java
+++ core/src/com/gameoff/game/managers/OverlayManager.java
@@ -1,9 +1,12 @@
 package com.gameoff.game.managers;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.gameoff.game.objects.ProgressTexture;
 import com.kyperbox.GameState;
+import com.kyperbox.controllers.AnimationController;
 import com.kyperbox.managers.StateManager;
 
 public class OverlayManager extends StateManager{
@@ -23,6 +26,16 @@ public class OverlayManager extends StateManager{
 		keyIcon = (Image) state.getUiLayer().getActor("key");
 		keyLabel = (Label) state.getUiLayer().getActor("keyLabel");
 		health = (ProgressTexture) state.getUiLayer().getActor("health");
+		
+		AnimationController healthAnim = new AnimationController();
+		
+		Animation a = state.getAnimation("circle");
+		if(a == null) {
+			state.storeAnimation("circle", state.createGameAnimation("circle", .33f)); 
+		}
+		
+		healthAnim.setAnimation("circle", PlayMode.LOOP);
+		health.addController(healthAnim);
 	}
 
 	@Override
